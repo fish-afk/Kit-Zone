da0119f8960b2b2a06a62b172b73fac0c45828e3
slide <mirzashihab2@gmail.com>
2022-01-29 12:33:25 +0200

    starting point

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
 
+const foo = (state) => {
+  return state + 1;
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='feed1'>
+      <button className='btn btn-primary buttons-group'>button</button>
+      <h1 className='badge badge-primary'>hello there!</h1>
+      <button>kjshfjhsf</button>
+      <h1>{foo(2)}</h1>
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
